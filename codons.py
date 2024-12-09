1468c0c7d0f980e9f1248d3349bd3a017ab13c83
Noam-Becker <noambecker1999@gmail.com>
2024-12-09 08:50:37 -0800

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,11 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
-
-
+  file= open(file_path)
+  rows= file.readlines()
+  file.close()
+  codon_dict={}
+  for row in rows:
+    row= row.strip().split('\t')
+    codon= row[0]
+    amino_acid= row[2]
+    codon_dict[codon]= amino_acid
+  return codon_dict
